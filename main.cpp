eb3df865bfef63e3a809327d17f1d38c09cd709c
Erik Martinez <61127579+ErikMtz99@users.noreply.github.com>
2020-04-13 20:26:43 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void Archivos(Actor actores[], Funcion funciones[], Pelicula peliculas[]) { // cree un void para que el main estuviera mas corto
+void Archivos(Actor actores[], Funcion funciones[], Pelicula peliculas[]) {
 	cout << peliculas[0].getListaActores(0).getID() << endl;
 	ifstream archivo_actores, archivo_peliculas;
 	archivo_actores.open("actores.txt");
@@ -38,7 +38,7 @@ void Archivos(Actor actores[], Funcion funciones[], Pelicula peliculas[]) { // c
 		archivo_peliculas >> a >> b >> c >> g >> numActores;
 
 
-		while (idx < numActores) { // checar esta parte porque no esta saliendo
+		while (idx < numActores) { 
 			archivo_peliculas >> ide;
 			int y = 0;
 			while (y < 20) {
@@ -64,17 +64,13 @@ void Archivos(Actor actores[], Funcion funciones[], Pelicula peliculas[]) { // c
 
 		j++;
 	}
+	
 	cout << endl;
 	cout << endl;
 	cout << endl;
 	cout << endl;
 
-	peliculas[9].getListaActores(0).muestra();
-
-	cout << peliculas[9].getDuracion() << endl;
-	cout << peliculas[9].getAnio() << endl;
-	cout << peliculas[9].getNumPeli() << endl;
-	cout << peliculas[9].getTitulo();
+	cout << peliculas[4].getTitulo() << endl;
 
 	archivo_actores.close();
 	archivo_peliculas.close();
@@ -86,14 +82,65 @@ int main() {
 	Pelicula peliculas[20];
 
 	Archivos(actores, funciones, peliculas);
+	int n, clave, sala;
+	string nombre;
+	int i = 0;
+	cout << "Ingrese el numero de funciones que habran por el dia: ";
+	cin >> n;
+	while (n < 1) {
+		cout << "Ingrese un numero mayor a 0 porfavor:  " << endl;
+		cin >> n;
+	}
 
-	//cout << "------------------" << endl;
+	while (i < n) {
 
-	//cout << peliculas[2].getCantActores() << endl;
-	//actores[2].muestra();
-	//cout << peliculas[1].getTitulo() << endl;
-	//
-	//peliculas[3].getListaActores(1).muestra();
+			cout << "Ingrese clave de la funcion: ";
+			cin >> clave;
+			bool Existe = false;
+			cout << endl;
+			for (int c = 0; c < 10; c++) {
+				if (clave == peliculas[c].getNumPeli())
+						{
+							Existe = true;
+							funciones[i].setNumPeli(clave);
+						}
+			}
+			while (Existe == false) {
+				cout << "Clave de pelicula no existe, ingrese denuevo: ";
+				cin >> clave;
+				for (int c = 0; c < 10; c++) {
+					if (clave == peliculas[c].getNumPeli())
+					{
+						Existe = true;
+					}
+				}
+			}
+
+			/*cout << "Ingrese numero de sala de la funcion: ";
+			cin >> sala;
+			cout << endl;
+			Existe = false;
+			for (int c = 0; c < n; c++) {
+				if (sala != funciones[c].getSala())
+				{
+					Existe = true;
+					funciones[i].setSala(sala);
+				}
+			}
+			while (Existe == false) {
+				cout << "Sala ocupada, ingrese otra: ";
+				cin >> sala;
+				for (int c = 0; c < n; c++) {
+					if (sala != funciones[c].getSala())
+					{
+						Existe = true;
+						funciones[i].setSala(sala);
+					}
+				}
+			}*/
+
+		i++;
+	}
 
 	return 0;
 }
